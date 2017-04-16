c680c7aaec6ad86298d73a2d9c91f234f4f9e0c3
Kevin Phung <phung.kevin@gmail.com>
2017-04-16 17:34:23 -0600

    add uglify plugin for production env

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 var env = process.env.NOVE_ENV;
 var config = {
@@ -6,11 +7,32 @@ var config = {
     entry: 'src/index.js',
     moduleName: 'libtest',
     plugins: [
+        nodeResolve({
+            jsnext: true
+        }),
         babel({
             plugins: ['external-helpers'],
             exclude: 'node_modules/**'
         })
     ]
+}
+
+if (env === 'production') {
+    config.plugins.push(uglify({
+        compress: {
+            pure_getters: true,
+            unsafe: true,
+            unsafe_comps: true,
+            warnings: false,
+            screw_ie8: false
+        },
+        mangle: {
+            screw_ie8: false
+        },
+        output: {
+            screw_ie8: false
+        }
+    }));
 }
 
 export default config;
